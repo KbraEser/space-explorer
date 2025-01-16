5f3b7f370bd3d30152752da645c519d12002cb43
KbraEser <kbra.demirbas@gmail.comm>
2025-01-16 23:14:34 +0300

    edit APOD scss properties

--- src/components/Apod.jsx
+++ src/components/Apod.jsx
@@ -11,13 +11,22 @@ const Apod = () => {
   if (!state.apodData) return <div className="loading">Loading...</div>;
 
   return (
-    <div className="header">
-      <h2>Günün Uzay Fotoğrafı (APOD - Astronomy Picture of the Day)</h2>
-      <img src={state.apodData.url} alt={state.apodData.title} />
+    <div className="container">
+      <h2 className="title">
+        Günün Uzay Fotoğrafı (APOD - Astronomy Picture of the Day)
+      </h2>
+      <div className="image-container">
+        <img src={state.apodData.url} alt={state.apodData.title} />
+        <p className="date">Date: {state.apodData.date}</p>
+      </div>
+      <div className="explanation">
+        <h3>{state.apodData.title}</h3>
+        <p>
+          <span>Explanation:</span> {state.apodData.explanation}
+        </p>
+      </div>
     </div>
   );
 };
 
 export default Apod;
-
-// {"date":"2025-01-16","explanation":"Beautiful and bright spiral galaxy M83 lies a some twelve million light-years away, near the southeastern tip of the very long constellation Hydra. Prominent spiral arms traced by dark dust lanes and blue star clusters lend this galaxy its popular name, The Southern Pinwheel.  Still, reddish star forming regions that dot this cosmic pinwheel's spiral arms have suggested another nickname, the Thousand-Ruby Galaxy. A mere 40,000 light-years across, smaller than the Milky Way, M83 is a member of a group of galaxies that includes active galaxy Centaurus A. In fact, the core of M83 itself is bright at x-ray energies, showing a high concentration of neutron stars and black holes left from an intense burst of star formation. This sharp color image also features spiky foreground Milky Way stars and distant background galaxies. The image data was captured with the Dark Energy Camera and Blanco 4-meter telescope at Cerro Tololo Inter-American Observatory.","hdurl":"https://apod.nasa.gov/apod/image/2501/noirlab2429a.jpg","media_type":"image","service_version":"v1","title":"M83: The Southern Pinwheel","url":"https://apod.nasa.gov/apod/image/2501/noirlab2429a_1024.jpg"}
